8e2e14ee6546f61304e52060177f48a7f768dd20
acthiago <95868097+acthiago@users.noreply.github.com>
2023-10-20 14:12:00 -0300

    Update teste2.py

--- git/teste2.py
+++ git/teste2.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # Defina os parâmetros da DAG
 default_args = {
-    'owner': 'seu_nome',
+    'owner': 'Thiago Cordeiro',
     'start_date': datetime(2023, 10, 20),
 }
 
@@ -13,11 +13,11 @@ dag = DAG('teste_dag', default_args=default_args, schedule_interval=None)
 
 # Define a função a ser executada pela tarefa
 def print_hello():
-    print("Olá, esta é uma DAG de teste!")
+    print("Olá, esta é uma DAG de teste!teste!teste!")
 
 # Cria uma tarefa que executa a função acima
 hello_task = PythonOperator(
-    task_id='print_hello',
+    task_id='teste de deg github',
     python_callable=print_hello,
     dag=dag,
 )
