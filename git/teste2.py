835034e7e4473a77f968e561ce8c262fa09fbcc9
acthiago <95868097+acthiago@users.noreply.github.com>
2023-10-20 14:06:05 -0300

    Create teste2.py

---
+++ git/teste2.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+# Defina os parâmetros da DAG
+default_args = {
+    'owner': 'seu_nome',
+    'start_date': datetime(2023, 10, 20),
+}
+
+# Crie a DAG
+dag = DAG('teste_dag', default_args=default_args, schedule_interval=None)
+
+# Define a função a ser executada pela tarefa
+def print_hello():
+    print("Olá, esta é uma DAG de teste!")
+
+# Cria uma tarefa que executa a função acima
+hello_task = PythonOperator(
+    task_id='print_hello',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+# Garanta a ordem das tarefas
+hello_task
